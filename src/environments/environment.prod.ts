63cb745394e3858571eaae716eab00a29ec4c8d1
Nicolas <nicolasbarada@gmail.com>
2021-02-04 22:31:30 -0300

    Se subio la web a Github Pages.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: "AIzaSyCCCIN5jSNWW_VsPSRqUjpVCEKEaz68uLg",
-    authDomain: "viviendas-oasis.firebaseapp.com",
-    projectId: "viviendas-oasis",
-    storageBucket: "viviendas-oasis.appspot.com",
-    messagingSenderId: "707920795618",
-    appId: "1:707920795618:web:09e421f536511ee65c4bbb"
-  }
+  // firebase: {
+  //   apiKey: "AIzaSyCCCIN5jSNWW_VsPSRqUjpVCEKEaz68uLg",
+  //   authDomain: "viviendas-oasis.firebaseapp.com",
+  //   projectId: "viviendas-oasis",
+  //   storageBucket: "viviendas-oasis.appspot.com",
+  //   messagingSenderId: "707920795618",
+  //   appId: "1:707920795618:web:09e421f536511ee65c4bbb"
+  // }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,14 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyCCCIN5jSNWW_VsPSRqUjpVCEKEaz68uLg",
-    authDomain: "viviendas-oasis.firebaseapp.com",
-    projectId: "viviendas-oasis",
-    storageBucket: "viviendas-oasis.appspot.com",
-    messagingSenderId: "707920795618",
-    appId: "1:707920795618:web:09e421f536511ee65c4bbb"
-  }
+  // firebase: {
+  //   apiKey: "AIzaSyCCCIN5jSNWW_VsPSRqUjpVCEKEaz68uLg",
+  //   authDomain: "viviendas-oasis.firebaseapp.com",
+  //   projectId: "viviendas-oasis",
+  //   storageBucket: "viviendas-oasis.appspot.com",
+  //   messagingSenderId: "707920795618",
+  //   appId: "1:707920795618:web:09e421f536511ee65c4bbb"
+  // }
 };
 
 /*
