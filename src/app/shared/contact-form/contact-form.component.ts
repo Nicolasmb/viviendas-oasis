2d363fc9c59f68efc4a6342bf56ccbfb9f4403fd
Nicolas <nicolasbarada@gmail.com>
2021-02-17 16:17:34 -0300

    Se agregó la interfaz gráfica del formulario de contacto en la pagina de modelos, falta implementar la funcionalidad.

---
+++ src/app/shared/contact-form/contact-form.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-form',
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.css']
+})
+export class ContactFormComponent implements OnInit {
+
+  nombre:   string;
+  email:    string;
+  telefono: string;
+  mensaje:  string;
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,12 @@
+// MODULOS
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'
+// COMPONENTES
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { WhatsappComponent } from './whatsapp/whatsapp.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 
 
@@ -11,14 +15,17 @@ import { WhatsappComponent } from './whatsapp/whatsapp.component';
     NavbarComponent,
     FooterComponent,
     WhatsappComponent,
+    ContactFormComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports: [
     NavbarComponent,
     FooterComponent,
-    WhatsappComponent
+    WhatsappComponent,
+    ContactFormComponent
   ]
 })
 export class SharedModule { }
