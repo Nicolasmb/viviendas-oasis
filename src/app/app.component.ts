1ffa33505b9fb44203908827ab9828c388548786
Nicolasmb <nicolasbarada@gmail.com>
2024-06-05 10:51:26 -0300

    Revert "Se quitó el popup inicial."
    This reverts commit 6b69a8d00d50d052eef5757ba7edaedb72bc6c80.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'viviendas-oasis';
+  public telefono = "541157292823";
+  public mensaje = "Hola!"
+
+  ngAfterViewInit(): void {
+    Swal.fire({
+      title: 'ALERTA!',
+      icon: 'warning',
+      html: `<div><p><span style="font-weight: bold">El número de Whatsapp 11-5637-9816 NO pertenece a la empresa Viviendas Oasis</span>.
+      Comuniquese con nosotros a través del botón de Whatsapp de esta página, nuestro formulario de contacto o personalmente en nuestras sucursales.</p></div>`,
+      showCloseButton: true,
+      confirmButtonColor: '#000000',
+      confirmButtonText: 'Cerrar'
+    })
+  }
+
 }
